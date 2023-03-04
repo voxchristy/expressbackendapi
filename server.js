32960f7788c0533c4d8bf842051a9d3995547a53
voxchristy <christy.thomas@live.com>
2023-03-05 01:33:09 +0530

    Created: Routes, Controllers, Middleware and DB setup

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const colors = require('colors');
+
+const connectDB = require('./config/db');
+
+//Route file import
+const bootcamps = require('./routes/bootcamps');
 
 //LOAD ENV VARS
 dotenv.config({path:'./config/config.env'});
 
+//Connect to Database
+connectDB();
+
 const app = express();
+
+//Middleware
+//app.use(logger);
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+//Mount Routes
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+const server = app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise)=>{
+    console.log(`Error: ${err.message}`.red.bold);
+    //close server and exit process
+    server.close(()=> process.exit(1));
+});
